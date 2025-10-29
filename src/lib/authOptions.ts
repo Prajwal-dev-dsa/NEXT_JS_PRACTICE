2790cb836b19fc1c4a47b781877bdc92d83adf69
Prajwal <prajwal77dwivedi@gmail.com>
2025-10-29 21:34:12 +0530

    NextAuth Authentication Added

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,69 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDB } from "./db";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+        if (!email || !password) {
+          throw new Error("Please provide email and password");
+        }
+        await connectToDB();
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("No user found");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          throw new Error("Invalid password");
+        }
+        return {
+          id: user._id.toString(),
+          email: user.email,
+          name: user.name,
+          image: user.image,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.image = user.image;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.email = token.email;
+        session.user.name = token.name;
+        session.user.image = token.image as string;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+};
+export default authOptions;
---
+++ src/next-auth.d.ts
@@ -0,0 +1,12 @@
+import { DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+export {};
